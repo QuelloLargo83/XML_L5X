738b06e0ea232a99f93a269f3c932a5eb26badec
Davide Capobianco <davide.capobianco@gea.com>
2023-08-24 13:41:01 +0200

    commenti fasi da rung comment inizia a funzionare

--- zzTRASH.py
+++ zzTRASH.py
@@ -1,11 +1,9 @@
-########################
-## NON FUNZIONA!! problemi di encoding! dovrebbe leggere
-###  le funzioni per la ricerca vanno bene
-##
+import l5x
+import cfg
 
-
-PlcFile = 'F:\SCRIPTING\XML_L5X\INI\PLC\STERILCAP\P16378_PLC_00P.L5X'
-text = '## D40_00.Production.Phase - MESSAGE FOR HMI ##'
+FilL5X = 'F:\SCRIPTING\XML_L5X\INI\PLC\FILLER\P16255_PLC_02C_M18.L5X'
+prj = l5x.Project(FilL5X)
+programs = prj.programs
 
 
 
@@ -20,6 +18,12 @@ def line_num_for_phrase_in_file(phrase, filename):
     return -1
 
 def GetPhCommentList(textToSearch,l5xFile):
+    """Ricava la parte relativa al testo cercato fino a </Comment>\n nel CDATA del file l5x
+
+    Args:
+        textToSearch (str): es: ## D40_00.Production.Phase - MESSAGE FOR HMI ##
+        l5xFile (str): percorso in cui cercare la stringa
+    """
 
     # ricavo il numero della linea all'interno del file in inizia la struttura dei commenti
     lnum = line_num_for_phrase_in_file(textToSearch,l5xFile)
@@ -29,7 +33,7 @@ def GetPhCommentList(textToSearch,l5xFile):
         lineList = f.readlines()[lnum:lnum+100]
         
         # ricavo l'indice di dove finisce l'area dei commenti del rung
-        commentIdx = lnum+100 #inizializzo per prova !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        commentIdx = lnum+100 #inizializzo per evitare errore quando non esiste il commento
         if '</Comment>\n' in lineList:
             commentIdx = lineList.index('</Comment>\n')
 
@@ -37,22 +41,50 @@ def GetPhCommentList(textToSearch,l5xFile):
 
 
 
-# leggo la lista dal file....ma non va bene...ci sono dei cazzilli!!!
-with open('F:\SCRIPTING\XML_L5X\STC_NomiCicliProd.txt','r',encoding='utf-8') as fileCicli:
-    ProdCycList = fileCicli.readlines()
+def GetPhaseComments(FileL5X,StructCicli,DesinenzaStruct,NomePOSPlc):
+    """Ricava un dizionario le cui chiavi sono i cicli (es: D40_00.Production.Phase) e i valori sono i commenti
+       dal file l5x prendendo dai commenti dei rung
+       ATTENZIONE: ad agosto 2023 nei commenti ci sono solo i .Phase
 
-for cycle in ProdCycList:
-    textComp ='## D40_00.' + cycle.replace('\n','') + '.Phase - MESSAGE FOR HMI ##'
-    comments = GetPhCommentList(textComp,PlcFile)
-  
-print(ord(text[10]))
-print(ord(textComp[10]))
+    Args:
+        FileL5X (str): percorso completo del file l5x in cui cercare i commenti delle fasi
+        StructCicli (str): es: D40_00
+        DesinenzaStruct (str): es: Phase, CycleMSG, PhaseMSG
+        NomePOSPlc (str): es FILLER, STERILCAP
 
-if text == textComp:
-    print('uguali')
-else:
-    print('diversi')
+    Returns:
+        dict: key: nomeCiclo, value: commenti
+    """
+
+########## RECUPERO I NOMI DEI CICLI NEL FILE L5X
+    # definisco un dizionario vuoto in cui mettere le variabili che mi interessano
+    cicli = {}
+    cicli = programs[NomePOSPlc].tags[StructCicli].value
     
+    # le chiavi sono i nomi del livello figlio della struttura
+    nomi_cicli = cicli.keys()
 
-# comments = GetPhCommentList(text,PlcFile)
-print(comments)
+    CycList =  list(nomi_cicli)
+
+#######   
+    # scorro i nomi dei cicli e li certo nel file l5x
+    comments = {}
+    for cycle in CycList:
+        nomeCiclo =  cycle.replace('\n','')
+        # text ='## D40_00.' + nomeCiclo + '.Phase - MESSAGE FOR HMI ##'
+        text ='## '+ StructCicli +'.' + nomeCiclo + '.'+ DesinenzaStruct +' - MESSAGE FOR HMI ##'
+        CommentStruct = GetPhCommentList(text,FileL5X)
+        if CommentStruct: # se manca la stringa nel posto in cui me lo aspetto, la struttura è vuota e quindi non la cago
+            comments[StructCicli +'.'+ nomeCiclo +'.'+ DesinenzaStruct] = CommentStruct[1:] #ogni chiave è il nome del ciclo piu caratteri per riconoscerla 
+        
+    return comments
+  
+
+
+
+
+
+ProdPhase = GetPhaseComments (FilL5X,cfg.INIREAD('plcprodcyclevar'),'Phase','FILLER')
+
+
+print(ProdPhase)
